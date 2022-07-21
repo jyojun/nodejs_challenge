3c5cf3c3b244712dd621e408f9e752fe172090bf
jyojun <parkcode98@gmail.com>
2022-07-21 22:14:55 +0900

    추가: free 함수

--- memory.js
+++ memory.js
@@ -41,6 +41,7 @@ class Memory {
         memory: type_length + padding,
         stackPointer: this.stackPointer,
       };
+      // 스택에서 스택 포인터가 가리키는 값이 heap의 주소값 이다.
       this.stack[this.stackPointer] = { heapAddress: this.heapAddress }; // 현재 스택 포인터에 heap의 주소를 넣는다.
       heapAddresses.push(this.heapAddress);
       // 메모리 힙에 할당 해줄 떄 마다, 스택 포인터는 더 위를 가리키고, heapAddress도 증가한다.
@@ -50,12 +51,22 @@ class Memory {
 
     return heapAddresses;
   }
-  free(pointer) {} // malloc 할 때 할당한 스택 주소값을 입력으로 받고, 스택 주소값에 있는 힙영역 고유주소를 찾아 해제 하고 반환한다.
+
+  // malloc 할 때 할당한 스택 포인터를 입력으로 받고, 스택 주소값에 있는 힙영역 고유주소를 찾아 해제 하고 반환한다.
+  free(pointer) {
+    // 스택에서 stack pointer가 가르키는 것은 memory 주소 를 저장
+    const result = this.stack[pointer];
+    result.heapAddress = null; // 주소도 null
+    this.memoryHeap[pointer] = null; // 메모리도 비워준다. null
+    return pointer;
+  }
   call(name, paramCount) {} // 스택 포인터에 포인터 변수를 paramCount만큼 반복하여 생성 -> 스택 포인터를 증가
   returnFrom(name) {} // 증가한 스택공간을 비우고 이전 호출 위치로 이동 ex) foo를 호출하면 foo를 name으로
   usage() {} // 스택 영역 (전체 크기, 사용중인 용량, 남은용량), 힙 영역 (전체크기, 사용 용량, 남은 용량) 배열로 리턴
   callstack() {} // 스택에 쌓인 호출 스택을 문자열로 리턴
-  heapdump() {} // 힙영역에서 사용중인 상태를 문자열 배열로 리턴
+  heapdump() {
+    return this.memoryHeap;
+  } // 힙영역에서 사용중인 상태를 문자열 배열로 리턴
   garbageCollect() {} // heap 영역에서 할당된 타입중 스택에 포인터 변수가 없는 경우를 해제
   reset() {} // stack, heap 공간을 비우고 init 상태
 }
