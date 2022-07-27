2fabdad7db12ea4bd1f6f791dfab5ef58ab22a98
jyojun <parkcode98@gmail.com>
2022-07-27 21:39:06 +0900

    수정: BoostSet

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 
 1. BoostSet class
    - [x] sum(other) : BoostSet에 다른 other boostset 요소들을 더해서 합집합을 리턴한다. 이미 같은 것이 있다면 추가하지 않는다. 
+   - [x] complement(other) : BoostSet에 다른 other BoostSet 요소를 빼서 여집합을 리턴한다.
+   - [x] intersect(other) : BoostSet와 다른 BoostSet 값과 비교하여, 두 집합에 모두 있는 원소 - 교집합을 리턴한다.
+   - [x] resultAll() : 모든 요소를 1차원 배열로 리턴한다.
 2. CountSet
 
 ## 학습 메모
 - Set.has(), Array.includes() 의 시간 복잡도 https://velog.io/@sozero/TIL-220307-Set.has-%EC%99%80-Array.includes-%EC%8B%9C%EA%B0%84%EB%B3%B5%EC%9E%A1%EB%8F%84
-- 
--- index.js
+++ index.js
@@ -1,43 +1,54 @@
 class BoostSet {
   #arr = [];
   constructor(arr) {
-    // 생성자 함수에서 set 역할을 해준다.
-    // arr.sort(function (a, b) {
-    //   return a - b;
-    // });
     let uniqueArr = arr.filter((item, idx) => {
       return arr.indexOf(item) === idx;
     });
+
+    Object.freeze(uniqueArr);
     this.#arr = uniqueArr;
   }
 
   #getArr() {
     return this.#arr;
-    j;
   }
 
   get arr() {
-    return this.#getArr();
+    const arr = this.#getArr();
+    return arr;
   }
 
+  // 합집합
   sum(other) {
-    let result = this.arr.filter((item) => {
-      return !other.arr.includes(item);
-    });
-    other.arr.map((item) => {
-      result.push(item);
+    let result = [...this.arr];
+    other.arr.map((x) => {
+      if (!result.includes(x)) result.push(x);
     });
     return result;
   }
-  complement(other) {}
-  intersect(other) {}
+
+  // 여집합
+  complement(other) {
+    return this.arr.filter((x) => !other.arr.includes(x));
+  }
+
+  // 교집합
+  intersect(other) {
+    return this.arr.filter((x) => other.arr.includes(x));
+  }
+
+  // 모든 요소 1차원 배열로 리턴
   resultAll() {}
 }
 
 const set = new BoostSet([1, 2, 2, 3, 3, 3, 3, 5]);
-const set2 = new BoostSet([1, 3, 3, 3]);
+const set2 = new BoostSet([1, 3, 3, 4, 4]);
 
 console.log(set.arr);
 console.log(set2.arr);
 
+set.arr[2] = 100;
+
 console.log(set.sum(set2));
+console.log(set.complement(set2));
+console.log(set.intersect(set2));
